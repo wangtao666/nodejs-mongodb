b0a6d7ed0edea7f6f891462b57be153b895bb7da
王涛 <wangtao@quanyou.com.cn>
2017-12-09 15:22:26 +0800

    增加了一个todo表单

--- firstblood/db/firstblood_schema.js
+++ firstblood/db/firstblood_schema.js
@@ -27,10 +27,17 @@ var newslistScheMa = new Schema({
     urls	 : String,
     numbers	 : Number,
 })
+//表三
+var todolistScheMa = new Schema({
+    label    : String,
+    content	 : String,
+})
 // 关联 userlist -> admins 表   表数据有问题，一切都白搭!
 //表一
 exports.userlist = db.model('admins', userlistScheMa,'admins');
 //表二
 exports.newslist = db.model('admin',newslistScheMa,'admin');
+//表三
+exports.todolist = db.model('todo',todolistScheMa,'todo');
 exports.db = db;
 console.log('数据库启动成功！！！！')
